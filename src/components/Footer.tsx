fc2aad846c4c60e41bca3a2ca1d0e30584086aa5
isaias-silva <zackblackmangold@gmail.com>
2024-01-15 23:50:54 -0300

    feat: :sparkles: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../public/logo.svg"
+import logoTwo from "../../public/logo-2.svg"
+export default function () {
+
+    return <footer className="flex flex-col absolute bottom-0 w-full  text-gray-50 px-7">
+        <div className="flex w-full justify-around">
+            <div className=" w-1/12">
+                <Image src={logoTwo} width={74} height={74} className=" rounded-lg" alt="logo" />
+                <Image src={logo} width={74} height={13} alt="logo" className="my-2" />
+            </div>
+            <div className="flex w-1/2">
+                <div className=" mx-3">
+                    <h4 className=" b">Cursos</h4>
+                    <ul>
+                        <li>
+                            <Link href={'#'} className="link">Valuation do Zero ao Avançado 2.0</Link>
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Código.py</Link>
+
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Minicurso Reels</Link>
+
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Enciclopédia de FII</Link>
+
+                        </li>
+                    </ul>
+                </div>
+                <div className=" mx-3">
+                    <h4 className=" b">Carteiras</h4>
+                    <ul>
+                        <li>
+                            <Link href={'#'} className="link">Carteira FATOR</Link>
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Carteira Seleção</Link>
+
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Carteira Essencial</Link>
+
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Carteira Small Caps</Link>
+
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Carteira Dividendos</Link>
+
+                        </li>   <li>
+                            <Link href={'#'} className="link">Carteira de FIIs</Link>
+
+                        </li>
+                    </ul>
+                </div>
+                <div className=" mx-3">
+                    <h4 className=" b">Sobre</h4>
+                    <ul>
+                        <li>
+                            <Link href={'#'} className="link">Quem somos</Link>
+                        </li>
+
+                    </ul>
+                </div>
+                <div className=" mx-3">
+                    <h4 className=" b">Redes Sociais</h4>
+                    <ul>
+                        <li>
+                            <Link href={'#'} className="link">Instagram</Link>
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Twitter</Link>
+                        </li>
+                        <li>
+                            <Link href={'#'} className="link">Youtube</Link>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+        <div>
+            <p>VAROS 2023</p>
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+
+}
